5663074375dfbfcb8fbb6769541319548efd9484
Alexdieu <alexdieuveult@gmail.com>
2025-02-24 21:53:31 +0200

    Update pub.js

--- pub.js
+++ pub.js
@@ -5,8 +5,11 @@ const RABBITMQ_URL = "amqp://localhost";
 async function sendAnnouncement(payload) {
   const connection = await amqp.connect(RABBITMQ_URL);
   const channel = await connection.createChannel();
+  //fanout : all chans 
   await channel.assertExchange(EXCHANGE, "fanout", { durable: true });
+  //read the messafe from the payload
   const messageBuffer = Buffer.from(JSON.stringify(payload));
+  //publish message
   channel.publish(EXCHANGE, "", messageBuffer);
   console.log("Announcement sent:", payload);
   await channel.close();
