13a30039d40576c0dc487504c6dd2b975e3c6ca4
Alexdieu <alexdieuveult@gmail.com>
2025-02-24 21:46:15 +0200

    Create producer.js

---
+++ producer.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+const QUEUE = "order_queue";
+const RABBITMQ_URL = "amqp://localhost";
+
+//Simple send an order function using Rabbit MQ to simulate
+async function sendOrder(order) {
+  const connection =  await amqp.connect(RABBITMQ_URL);
+  //Connect to rabbit mq
+  const channel =  await connection.createChannel();
+  await channel.assertQueue(QUEUE, {  durable: true  });
+  const messageBuffer = Buffer.from(JSON.stringify(order));
+  channel.sendToQueue(QUEUE, messageBuffer, {  persistent: true });
+  console.log("Sent order:", order);
+  await channel.close();
+  await connection.close();
+  //connection over order sent
+}
+
+// Simulate a order from web form
+sendOrder({ orderId: 101, customerName: "Pablo Arce", items: ["Computer", "mouse"] });
